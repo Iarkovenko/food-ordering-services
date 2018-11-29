62db858535b282c44685f01b57f5c6e18f99f853
Iarkovenko <yarkovenko@ukr.net>
2018-11-30 01:18:04 +0300

    added close modal with Esc

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
 import React, { Fragment } from 'react';
 import Header from './header/Header';
-import OrderHistory from './order-history';
-import MenuList from './menu-card/MenuList';
-import SignUp from './authform/signup';
-import SignIn from './authform/signin';
-import dataOrderHistory from '../data/order-history.json';
+// import OrderHistory from './order-history';
+// import MenuList from './menu-card/MenuList';
+// import SignUp from './authform/signup';
+// import SignIn from './authform/signin';
+// import dataOrderHistory from '../data/order-history.json';
 
 const App = () => (
   <Fragment>
     <Header />
-    <OrderHistory dataOrderHistory={dataOrderHistory} />
-    <MenuList />
-    <SignUp />
-    <SignIn />
   </Fragment>
 );
 
